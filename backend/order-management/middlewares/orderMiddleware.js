7291209f4aa0453cf2ef0bd41b018cd31da64429
Clubfuzzbutt <99827391+Clubfuzzbutt@users.noreply.github.com>
2024-03-20 11:49:10 +0100

    integrated with middleware

--- backend/order-management/middlewares/orderMiddleware.js
+++ backend/order-management/middlewares/orderMiddleware.js
@@ -1,1 +1,15 @@
 // Handle authentication and validation.
+
+const express = require('express');
+const app = express();
+const orderRoutes = require ('/routes/orderRoutes');
+
+//middleware 
+app.use(express.json());
+
+//routes
+app.use('/api', orderRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => console.log('Server running on port ${PORT}'));
--- backend/order-management/routes/orderRoutes.js
+++ backend/order-management/routes/orderRoutes.js
@@ -3,3 +3,19 @@
 const express = require ('express');
 const router = express.Router();
 const orderController = require ('/controller/orderController');
+
+router.post('/orders', async (req, res) => {
+    try {
+        const {user_id, status, total_price} = req.body;
+        const orders = await orderController.creaedOrder(user_id, status, total_price);
+        res.status(201).json(orders);
+    }   catch (error) {
+        res.status(500).json({error:error.message});
+    }
+
+} );
+
+// Implement other routes as needed
+module.exports = router;
+
+
