8d2832c35d08a8a2ce81a29462efe374bc207923
Clubfuzzbutt <99827391+Clubfuzzbutt@users.noreply.github.com>
2024-03-20 19:00:06 +0100

    Began adding stripe in the controller layer

--- backend/order-management/routes/orderRoutes.js
+++ backend/order-management/routes/orderRoutes.js
@@ -26,8 +26,14 @@ router.get('/orders/:user_id', async (req, res) => {
     }
 });
 
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controller/orderController');
+//Route for creating a payment intent
+router.post('/create-payment-intent', orderController.createPaymentIntent);
+
 
 // Implement other routes as needed
 module.exports = router;
 
-
+//from frontend we will have to call the '/create-payment-intent' endpoint to retrieve the client secret and use it to confirm the payment with stripe o nthe client side.
