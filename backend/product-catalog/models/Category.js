abc6e6f8ffc630402a804a26454f96fe65fd1cfb
Fathionsons <gathefathe29@gmail.com>
2024-03-21 12:19:50 +0100

    ProductCatelog
    adding Category , get and search functions

---
+++ backend/product-catalog/models/Category.js
@@ -0,0 +1,18 @@
+class Category {
+    static async findByNameOrDescription(search) {
+      let query = supabase
+        .from('Categories')
+        .select('*');
+  
+      if (search) {
+        query = query
+          .ilike('name', `%${search}%`)
+          .or(`description.ilike.%${search}%`);
+      }
+  
+      const { data, error } = await query;
+      if (error) throw error;
+      return data;
+    }
+  }
+  
